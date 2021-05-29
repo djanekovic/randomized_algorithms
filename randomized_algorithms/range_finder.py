ae324d8b57fd51c8de79d230495ef073613467d0
Darko Janekovic <darko.janekovic@gmail.com>
2021-05-29 19:03:43 +0200

    range finder tests passing
    Signed-off-by: Darko Janekovic <darko.janekovic@gmail.com>

--- randomized_algorithms/range_finder.py
+++ randomized_algorithms/range_finder.py
@@ -1,38 +1,68 @@
 import numpy as np
 import scipy.linalg
+from time import time
 
 from random import sample
 from abc import ABC, abstractmethod
 
 
 #{{{ RandomizedRangeFinders
+class BaseRangeFinder:
+    def __init__(self, A, k=0, p=10, q=0, check_finite=True, debug=True):
+        m, n = A.shape
+        self._duration = {}
 
-def RandomizedRangeFinder(A, k=0, p=10, q=0, check_finite=True, svd=False,
-                          return_random_and_sample=False, debug=True):
-    m, n = A.shape
+        # if k is set leave it as is, else set to min(m, n)
+        k = k if k else min(m, n)
+        self._l = min(k + p, min(m, n))
 
-    # if k is set leave it as is, else set to min(m, n)
-    k = k if k else min(m, n)
-    l = min(k + p, min(m, n))
+        # form n x l Gaussian random matrix G
+        self._G = np.random.normal(size=(n, self._l))
 
-    # form n x l Gaussian random matrix G
-    G = np.random.normal(size=(n, l))
+        self._Q = self._compute(A, q, debug=debug)
 
-    # Form the sample matrix m x l
-    Y = A @ G
-    for _ in range(q):
-        Z = A.T @ Y
-        Y = A @ Z
+    @property
+    def G(self):
+        return self._G
 
-    # Q is mxl
-    Q, _ = np.linalg.qr(Y)
+    @property
+    def Y(self):
+        return self._Y
 
-    if return_random_and_sample:
-        return Q, G, Y
+    @property
+    def Q(self):
+        return self._Q
 
-    return Q
+    @property
+    def duration(self):
+        return self._duration
 
 
+class RandomizedRangeFinder(BaseRangeFinder):
+    def __init__(self, A, k=0, p=10, q=0, check_finite=True, debug=True):
+        super().__init__(A, k, p, q)
+
+    def _compute(self, A, q, **kwargs):
+
+        start = time()
+
+        # Form the sample matrix m x l
+        self._Y = A @ self._G
+        for _ in range(q):
+            Z = A.T @ self._Y
+            self._Y = A @ Z
+        self._duration["gemm"] = time() - start
+
+        start = time()
+
+        # Q is mxl
+        Q, _ = np.linalg.qr(self._Y)
+        self._duration["qr"] = time() - start
+
+        return Q
+
+
+#TODO: move to the GPU branch
 def GPURandomizedRangeFinder(A, k=0, p=10, q=0, debug=True):
     import cupy as cp
     m, n = A.shape
@@ -62,30 +92,24 @@ def GPURandomizedRangeFinder(A, k=0, p=10, q=0, debug=True):
 
 #{{{ RandomizedSubspaceIteration
 
-def RandomizedSubspaceIteration(A, k=0, p=10, q=0, return_random_and_sample=False, debug=True):
-    m, n = A.shape
+class RandomizedSubspaceIteration(BaseRangeFinder):
+    def __init__(self, A, k=0, p=10, q=0, check_finite=True, debug=True):
+        super().__init__(A, k, p, q)
 
-    # if k is set leave it as is, else set to min(m, n)
-    k = k if k else min(m, n)
-    l = min(k + p, min(m, n))
+    def _compute(self, A, q, **kwargs):
+        start = time()
 
-    # form n x l Gaussian random matrix G
-    G = np.random.normal(size=(n, l))
+        # Orhonormalize the columns
+        Q, _ = np.linalg.qr(A @ self._G, 'reduced')
 
-    # Form the sample matrix m x l
-    Y = A @ G
+        for _ in range(q):
+            W, _ = np.linalg.qr(A.conj().T @ Q, 'reduced')
+            Q, _ = np.linalg.qr(A @ W, 'reduced')
+        self._duration['gemm'] = time() - start
 
-    # Orhonormalize the columns
-    Q, _ = np.linalg.qr(Y, 'reduced')
+        self._duration['qr'] = 0
 
-    for _ in range(q):
-        W, _ = np.linalg.qr(A.conj().T @ Q, 'reduced')
-        Q, _ = np.linalg.qr(A @ W, 'reduced')
-
-    if return_random_and_sample:
-        return Q, G, Y
-
-    return Q
+        return Q
 
 
 def GPURandomizedSubspaceIteration(A, k=0, p=10, q=0, debug=True):
@@ -115,49 +139,56 @@ def GPURandomizedSubspaceIteration(A, k=0, p=10, q=0, debug=True):
 
 #{{{ FastRandomizedRangeFinder
 
-def FastRandomizedRangeFinder(A, k=0, p=20, debug=True):
-    m, n = A.shape
+#TODO: can we think of some magic to treat this as RangeFinder
+class FastRandomizedRangeFinder():
+    def __init__(self, A, k=0, p=10, q=0, check_finite=True, debug=True):
+        assert q is 0
+        self._duration = {}
+        m, n = A.shape
 
-    # if k is set leave it as is, else set to min(m, n)
-    k = k if k else min(m, n)
-    l = min(k + p, min(m, n))
+        # if k is set leave it as is, else set to min(m, n)
+        k = k if k else min(m, n)
+        l = min(k + p, min(m, n))
 
-    # Compute Y = A Omega where Omega = sqrt(n/k+p) D F R.
-    # - D is an nxn diagonal matrix whose entries are independent random variables uniformly
-    #   distributed on the complex unit circle
-    # - F is the nxn unitary DFT matrix
-    # - R is an nxk+p matrix whose columns are drawn randomly without replacement from the
-    #   columns of the nxn identity matrix.
-    # we want to compute Y as fast as possible!
-    # Since F is DFT matrix we can apply it in O(nlogn), notice D and R have O(n) nonzero
-    # elements.
-    #
-    # Y = A Omega
-    # Y.T = Omega.T A.T
-    # Y.T = norm R.T F.T D.T A.T    -> F == F.T, D == D.T
-    # Y.T = norm R.T F D A.T
-    #
-    # Since we are dealing with matrices that have O(n) nonzeros we have to smart how we
-    # handle them.
-    # Action of matrix R.T on some other matrix X is simple row extraction:
-    #
-    # |1 0 0| |a b| = |a b|
-    # |0 0 1| |c d|   |e f|
-    #         |e f|
-    #
-    #
-    #TODO: profile why is this so slow...
-    R = sample(list(range(n)), l)
-    D = np.exp(2 * 1j * np.random.random_sample(size=n) * np.pi)
+        self._R = sample(list(range(n)), l)
+        self._D = np.sqrt(n/l) * np.exp(2 * 1j * np.random.random_sample(size=n) * np.pi)
 
-    # Y.T is lxm => Y is mxl
-    Yt = np.sqrt(n/l) * np.fft.fft(np.multiply(D[:, None], A.T), norm='ortho', axis=0)[R, :]
-    if debug:
-        import scipy.linalg
-        Y_ = np.sqrt(n/l) * A @ np.diagflat(D) @ scipy.linalg.dft(n, scale='sqrtn') @ np.eye(n)[:, R]
-        assert np.allclose(Y_.T, Yt)
+        start = time()
+        self._Y = self._compute(A, debug)
+        self._duration['gemm'] = time() - start
 
-    return Yt.T
+        start = time()
+        self._Q, _ = np.linalg.qr(self._Y)
+        self._duration['qr'] = time() - start
+
+    def _compute(self, A, debug):
+        Yt = np.fft.fft(np.multiply(self._D[:, None], A.T), norm='ortho', axis=0)[self._R, :]
+
+        if debug:
+            _, n = A.shape
+            Y_ = A  @ np.diagflat(self._D)
+            Y_ = Y_ @ scipy.linalg.dft(n, scale='sqrtn')
+            Y_ = Y_ @ np.eye(n)[:, self._R]
+            assert np.allclose(Y_, Yt.T)
+
+        return Yt.T
+
+    @property
+    def Y(self):
+        return self._Y
+
+    @property
+    def G(self):
+        n = len(self._D)
+        return np.diagflat(self._D) @ scipy.linalg.dft(n, scale='sqrtn') @ np.eye(n)[:, self._R]
+
+    @property
+    def Q(self):
+        return self._Q
+
+    @property
+    def duration(self):
+        return self._duration
 
 
 def GPUFastRandomizedRangeFinder(A, k=0, p=20, debug=True):
@@ -180,48 +211,40 @@ def GPUFastRandomizedRangeFinder(A, k=0, p=20, debug=True):
 
 #{{{ BlockRandomizedRangeFinder
 
-def BlockRandomizedRangeFinder(A, k=0, p=10, q=0, col_num=2, check_finite=True,
-                               return_random_and_sample=False, debug=True):
-    m, n = A.shape
+class BlockRandomizedRangeFinder(BaseRangeFinder):
+    def __init__(self, A, k=0, p=10, q=0, col_num=2, check_finite=True, debug=True):
+        self._col_num = col_num
 
-    # if k is set leave it as is, else set to min(m, n)
-    k = k if k else min(m, n)
-    l = min(k + p, min(m, n))
+        super().__init__(A, k, p, q)
 
-    # form n x l Gaussian random matrix G
-    G = np.random.normal(size=(n, l))
+    def _compute(self, A, q, debug=True):
+        m, n = A.shape
 
-    # number of columns we are pushing in the pipeline
-    current_column = 0
+        start = time()
 
-    Y = np.zeros((m, l))
+        self._Y = np.zeros((m, self._l))
+        for current_column in range(0, n, self._col_num):
+            a = A[:, current_column:current_column + self._col_num]
+            g = self._G[current_column:current_column + self._col_num]
 
-    for current_column in range(0, n, col_num):
-        a = A[:, current_column:current_column + col_num]
-        g = G[current_column:current_column + col_num]
+            y = a @ g
 
-        y = a @ g
+            if q == 0:
+                self._Y += y
 
-        if q == 0:
-            Y += y
+            for _ in range(q):
+                self._Y += np.outer(a, a) @ y
+        self._duration['gemm'] = time() - start
 
-        for _ in range(q):
-            Y += np.outer(a, a) @ y
+        # Here we have all the columns and Y should be A @ G
+        if debug:
+            assert np.allclose(self._Y, A @ self._G)
 
-    # Here we have all the columns and Y should be A @ G
-    if debug:
-        assert np.allclose(Y, A @ G)
+        start = time()
+        Q, _ = np.linalg.qr(self._Y)
+        self._duration['qr'] = time() - start
 
-    overwrite_a = False if return_random_and_sample else True
-    Q, _ = scipy.linalg.qr(Y, mode='economic', overwrite_a=overwrite_a,
-                           check_finite=check_finite)
-
-    if return_random_and_sample:
-        return Q, G, Y
-
-    return Q
-
-
+        return Q
 
 def GPUBlockRandomizedRangeFinder(A, k=0, p=10, q=0, col_num=10, debug=True):
     import cupy as cp
--- tests/test_range_finder.py
+++ tests/test_range_finder.py
@@ -1,17 +1,25 @@
 import unittest
 import numpy as np
+from functools import wraps
 
 from randomized_algorithms import (BlockRandomizedRangeFinder, RandomizedRangeFinder,
                                    RandomizedSubspaceIteration, FastRandomizedRangeFinder)
 
 
-class TestRangeFinder(unittest.TestCase):
-    def check_error_bound(self, A, Q):
-        m, n = A.shape
-        _, k = Q.shape
+m = 2048
+n = 1024
+k = n-10
 
-        Omega = np.random.normal(size=(n, k))
-        U, D, Vh = np.linalg.svd(A, full_matrices=False)
+
+class BaseRandomizedRangeFinder(unittest.TestCase):
+    def setUp(self):
+        self.A = np.random.randn(m, n)
+
+    def tearDown(self):
+        Omega = self._rf.G
+        Q = self._rf.Q
+
+        U, D, Vh = np.linalg.svd(self.A, full_matrices=False)
 
         Omega_1 = Vh[:k] @ Omega
         Omega_2 = Vh[k:] @ Omega
@@ -19,48 +27,30 @@ class TestRangeFinder(unittest.TestCase):
         Sigma_1 = np.diagflat(D[:k])
         Sigma_2 = np.diagflat(D[k:])
 
-        return np.linalg.norm(A - Q @ Q.conj().T @ A)**2 <= (
+        condition = np.linalg.norm(self.A - Q @ Q.conj().T @ self.A)**2 <= (
                 (np.linalg.norm(Sigma_2)**2) +
                 (np.linalg.norm(Sigma_2 @ Omega_2 @ np.linalg.pinv(Omega_1)))**2)
 
+        self.assertTrue(condition)
 
 
-m = 2048
-n = 1024
-k = n-10
-
-class TestRandomizedRangeFinder(TestRangeFinder, unittest.TestCase):
+class TestRandomizedRangeFinder(BaseRandomizedRangeFinder):
     def test_error_bound(self):
-        A = np.random.normal(size=(m, n))
-
-        Q = RandomizedRangeFinder(A, k=k, p=0, q=0)
-
-        self.assertTrue(self.check_error_bound(A, Q))
+        self._rf = RandomizedRangeFinder(self.A, k=k, p=0, q=0)
 
 
-class TestRandomizedSubspaceIteration(TestRangeFinder, unittest.TestCase):
+class TestRandomizedSubspaceIteration(BaseRandomizedRangeFinder):
     def test_error_bound(self):
-        A = np.random.normal(size=(m, n))
-
-        Q = RandomizedSubspaceIteration(A, k=k, p=0, q=0)
-
-        self.assertTrue(self.check_error_bound(A, Q))
+        self._rf = RandomizedSubspaceIteration(self.A, k=k, p=0, q=0)
 
 
-class TestFastRandomizedRangeFinder(unittest.TestCase):
+class TestBlockRandomizedRangeFinder(BaseRandomizedRangeFinder):
     def test_error_bound(self):
-        A = np.random.normal(size=(m, n))
+        self._rf = BlockRandomizedRangeFinder(self.A, k=k, p=0, q=0)
 
-        Y = FastRandomizedRangeFinder(A, k=k, p=0)
-
-
-class TestBlockRandomizedRangeFinder(TestRangeFinder, unittest.TestCase):
+class TestFastRandomizedRangeFinder(BaseRandomizedRangeFinder):
     def test_error_bound(self):
-        A = np.random.normal(size=(m, n))
-
-        Q = BlockRandomizedRangeFinder(A, k=k, p=0, q=0)
-
-        self.assertTrue(self.check_error_bound(A, Q))
+        self._rf = FastRandomizedRangeFinder(self.A, k=k, p=0)
 
 if __name__ == "__main__":
     unittest.main()
